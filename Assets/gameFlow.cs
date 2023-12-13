a2bc2bab761fcaa524b22829a861699ed4ae57bb
ashmasood <24100018@lums.edu.pk>
2023-12-13 14:37:33 +0500

    menu, winscene, losescene done

--- Assets/gameFlow.cs
+++ Assets/gameFlow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class gameFlow : MonoBehaviour
 {
 
-    // public static int[] orderValue = new int[] { 1, 2, 3, 2, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    // public static int[] orderValue = new int[] { 1, 2, 7, 2, 4, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     public static int[] plateValue = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     public static int idx = 0; 
     
